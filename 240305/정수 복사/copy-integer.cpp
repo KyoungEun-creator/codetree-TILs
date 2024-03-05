5e156e655cff9dce7788e563d69a4a8263a9def7
Joe <kavvy99@naver.com>
2024-03-05 10:17:01 +0900

    Title: 정수 복사, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/정수 복사/copy-integer.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 3, b = 4;
+    b = a;
+    int c = a * b;
+
+    cout << a << " ";
+    cout << b << endl;
+    cout << c;
+
+    return 0;
+}

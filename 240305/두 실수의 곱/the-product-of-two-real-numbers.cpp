28b5e517299a9dffd706690f07dc80649f5d7ad3
Joe <kavvy99@naver.com>
2024-03-05 10:04:38 +0900

    Title: 두 실수의 곱, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240305/두 실수의 곱/the-product-of-two-real-numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << fixed;
+
+    double a = 5.26;
+    double b = 8.27;
+    double c = a * b;
+
+    cout.precision(3);
+    cout << c;
+
+    return 0;
+}

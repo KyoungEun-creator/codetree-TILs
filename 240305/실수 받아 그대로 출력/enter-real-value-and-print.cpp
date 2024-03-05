9e6ec8c8b5dcb2d5b3eb976014bc7977dfaa50ed
Joe <kavvy99@naver.com>
2024-03-05 17:29:22 +0900

    Title: 실수 받아 그대로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240305/실수 받아 그대로 출력/enter-real-value-and-print.cpp
+++ 240305/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -3,12 +3,11 @@ using namespace std;
 
 int main() {
     
-    cout << fixed;
-
     double n;
     cin >> n;
-    cout.precision(2);
 
+    cout << fixed;
+    cout.precision(2);
     cout << n;
 
     return 0;

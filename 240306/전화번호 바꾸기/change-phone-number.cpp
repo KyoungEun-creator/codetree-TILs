76a549caf6215ca617d2f47f55543d79188347e5
Joe <kavvy99@naver.com>
2024-03-06 15:40:55 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240306/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    
+    int a, b, c;
+    char dash;
+
+    cin >> a >> dash >> b >> dash >> c;
+
+    cout << "010" << "-" << c << "-" << b;
+
+    return 0;
+}

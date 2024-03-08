07ff59790004a799f7123ee3adcb464b8696ee78
Joe <kavvy99@naver.com>
2024-03-08 16:00:28 +0900

    Title: 알파벳에 따른 평가, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240308/알파벳에 따른 평가/evaluation-by-alphabet.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    string grade;
+    cin >> grade;
+
+    if (grade == "S")
+        cout << "Superior";
+    else if (grade == "A")
+        cout << "Excellent";
+    else if (grade == "B") 
+        cout << "Good";
+    else if (grade == "C") 
+        cout << "Usually";
+    else if (grade == "D")
+        cout << "Effort";
+    else
+        cout << "Failure";
+    return 0;
+}

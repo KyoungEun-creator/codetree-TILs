6bad2a43e3decd95f20d83a242932d11e61e7bc1
Joe <kavvy99@naver.com>
2024-03-08 15:57:50 +0900

    Title: 등급 매기기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240308/등급 매기기/ranking.cpp
+++ 240308/등급 매기기/ranking.cpp
@@ -6,19 +6,15 @@ int main() {
     int score;
     cin >> score;
 
-    if (score >= 90) {
+    if (score >= 90) 
         cout << "A";
-    }
-    else if (score >= 80) {
+    else if (score >= 80)
         cout << "B";
-    }
-    else if (score >= 70) {
+    else if (score >= 70)
         cout << "C";
-    }
-    else if (score >= 60) {
+    else if (score >= 60)
         cout << "D";
-    }
     else 
         cout << "F";
-        return 0;
+    return 0;
 }

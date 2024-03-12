c0bd7f433deb5be29b3625d0d4f1b641522a2ba3
Joe <kavvy99@naver.com>
2024-03-12 17:56:32 +0900

    Title: n까지 숫자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240312/n까지 숫자 출력/print-1-to-n.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin >> n;
+
+    int a;
+    a = 1;
+
+    while (a <= n) {
+        cout << a << " ";
+        a++;
+    }
+    return 0;
+}

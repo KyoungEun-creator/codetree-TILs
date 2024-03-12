7fbac7e44598544749ff7f1f6a447f24a4434597
Joe <kavvy99@naver.com>
2024-03-12 15:03:06 +0900

    Title: 홀수만 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240312/홀수만 출력/output-only-odd.cpp
+++ 240312/홀수만 출력/output-only-odd.cpp
@@ -5,19 +5,6 @@ int main() {
 
     int a, b;
     cin >> a >> b;
-    
-    if (a % 2 == 0) {
-        a += 1;
-    }
-    else {
-        a = a;
-    }
-    if (b % 2 == 0) {
-        b += 1;
-    }
-    else {
-        b = b;
-    }
 
     for (int i = a; i <= b; i += 2) {
         cout << i << " ";

fe7a4eb7e152410ebc7683af5faddfe52177491f
Joe <kavvy99@naver.com>
2024-03-15 10:56:22 +0900

    Title: 369 게임, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240315/369 게임/369-game.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n, i;
+    i = 1;
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        if (i % 3 == 0) {
+            cout << "0" << " ";
+        }
+        else if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9 ) {
+            cout << "0" << " ";
+        }
+        else if (i - (i % 10) == 30 || i - (i % 10) == 60 || i - (i % 10) == 90) {
+            cout << "0" << " ";
+        }
+        else {
+            cout << i << " ";
+        }
+    }
+    return 0;
+}

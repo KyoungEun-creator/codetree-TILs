4da2f222f1b5c685e2795a74cb56fdeb96dfe725
Joe <kavvy99@naver.com>
2024-03-15 10:47:47 +0900

    Title: a 부터 b 까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240315/a 부터 b 까지/a-to-b.cpp
+++ 240315/a 부터 b 까지/a-to-b.cpp
@@ -6,18 +6,30 @@ int main() {
     int a, b;
     cin >> a >> b;
 
-    cout << a << " ";
+    // cout << a << " ";
 
-    for (int i = a; i < b;) {
+    // for (int i = a; i < b;) {
+    //     if (i % 2 == 1) {
+    //         i = i * 2;
+    //     }
+    //     else if (i % 2 == 0) {
+    //         i = i + 3;
+    //     }
+    //     if (i <= b) {
+    //         cout << i << " ";
+    //     }
+    // }
 
+    int i;
+    i = a;
+
+    while (i <= b) {
+        cout << i << " ";
         if (i % 2 == 1) {
-            i = i * 2;
+            i *= 2;
         }
-        else if (i % 2 == 0) {
-            i = i + 3;
-        }
-        if (i <= b) {
-            cout << i << " ";
+        else {
+            i += 3;
         }
     }
     return 0;

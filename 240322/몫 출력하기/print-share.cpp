f515239422e2a74431e92f03a64b660394013024
Joe <kavvy99@naver.com>
2024-03-22 09:59:07 +0900

    Title: 몫 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240322/몫 출력하기/print-share.cpp
+++ 240322/몫 출력하기/print-share.cpp
@@ -6,14 +6,19 @@ int main() {
     int count = 0;
 
     while(true) {
+
         int n;
         cin >> n;
-        if (n % 2 == 0) {
-            cout << n / 2 << endl;
-            count++;
-            if (count == 3) {
-                break;
-            }
+
+        if (n % 2 == 1) {
+            continue;
+        }
+
+        cout << n / 2 << endl;
+        count++;
+
+        if (count == 3) {
+            break;
         }
     }
     return 0;

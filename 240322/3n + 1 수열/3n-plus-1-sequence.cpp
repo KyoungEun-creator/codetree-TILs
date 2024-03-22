60155dc0b68e8c3711c2798a8d16861b1b4c2441
Joe <kavvy99@naver.com>
2024-03-22 11:46:41 +0900

    Title: 3n + 1 수열, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240322/3n + 1 수열/3n-plus-1-sequence.cpp
+++ 240322/3n + 1 수열/3n-plus-1-sequence.cpp
@@ -7,6 +7,10 @@ int main() {
     cin >> n;
 
     while(true) {
+
+        if (n == 1) {
+            break;
+        }
         
         if (n % 2 == 0) {
             n = n / 2;
@@ -15,9 +19,6 @@ int main() {
         else if (n % 2 == 1) {
             n = n * 3 + 1;
             cnt++;
-        }
-        if (n == 1) {
-            break;
         }
     }
     cout << cnt;

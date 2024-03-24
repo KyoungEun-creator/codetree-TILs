e9d6b7d9e1c0813715b47c398c9ea8eba7dcde4b
Joe <kavvy99@naver.com>
2024-03-24 21:05:36 +0900

    Title: 정사각형 두 개 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240324/정사각형 두 개 출력/output-two-rectangles.cpp
+++ 240324/정사각형 두 개 출력/output-two-rectangles.cpp
@@ -6,14 +6,20 @@ int main() {
     int n;
     cin >> n;
 
-    for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < n; j++) {
-            for (int k = 0; k < n; k++) {
-                cout << "*";
-            }
-            cout << endl;
+    for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) {
+            cout << "*";
         }
         cout << endl;
     }
+    cout << endl;
+    
+        for (int j = 0; j < n; j++) {
+        for (int k = 0; k < n; k++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+    
     return 0;
 }

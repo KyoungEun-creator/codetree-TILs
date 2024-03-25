856333ea4751dda59a43dc80e9adfc14a3ef4e4f
Joe <kavvy99@naver.com>
2024-03-25 10:22:34 +0900

    Title: 직각 삼각형 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240325/직각 삼각형 출력/Right-triangle-and-output.cpp
+++ 240325/직각 삼각형 출력/Right-triangle-and-output.cpp
@@ -6,8 +6,16 @@ int main() {
     int n;
     cin >> n;
 
-    for (int i = 1; i <= n; i++) {
-        for (int j = 1; j <= 2 * i - 1; j++) {
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= 2 * i - 1; j++) {
+    //         cout << "*";
+    //     }
+    //     cout << endl;
+    // }
+    // return 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 2 * i + 1; j++) {
             cout << "*";
         }
         cout << endl;

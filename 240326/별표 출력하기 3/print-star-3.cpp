e34f37d443092a17f17baa5c4a39133b8e2c8a9e
Joe <kavvy99@naver.com>
2024-03-26 10:46:15 +0900

    Title: 별표 출력하기 3, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240326/별표 출력하기 3/print-star-3.cpp
+++ 240326/별표 출력하기 3/print-star-3.cpp
@@ -6,11 +6,21 @@ int main() {
     int n;
     cin >> n;
     
-    for (int i = n; i > 0; i--) {
-        for (int j = i; j <= n - 1; j++) {
+    // for (int i = n; i > 0; i--) {
+    //     for (int j = i; j <= n - 1; j++) {
+    //         cout << "  ";
+    //     }
+    //     for (int j = 2 * i - 1; j > 0; j--) {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
             cout << "  ";
         }
-        for (int j = 2 * i - 1; j > 0; j--) {
+        for (int j = 0; j < (2 * n) - (2 * i) - 1; j++) {
             cout << "* ";
         }
         cout << endl;

6e181260ada1dd742afa55e5b4c9ec9dc49a7249
Joe <kavvy99@naver.com>
2024-03-27 10:19:41 +0900

    Title: 별표 출력하기 6, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240327/별표 출력하기 6/print-star-6.cpp
+++ 240327/별표 출력하기 6/print-star-6.cpp
@@ -6,21 +6,41 @@ int main() {
     int n;
     cin >> n;
 
-    for (int i = n; i > 0; i--) {
-        for (int j = 0; j < n - i; j++) {
+    // for (int i = n; i > 0; i--) {
+    //     for (int j = 0; j < n - i; j++) {
+    //         cout << "  ";
+    //     }
+    //     for (int j = 0; j < 2 * i - 1; j++) {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // for (int i = 1; i < n; i++) {
+    //     for (int j = 0; j < n - i - 1; j++) {
+    //         cout << "  ";
+    //     }
+    //     for (int j = 0; j < 2 * i + 1; j++) {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
             cout << "  ";
         }
-        for (int j = 0; j < 2 * i - 1; j++) {
+        for (int j = 0; j < (2 * n) - (2 * i) - 1; j++) {
             cout << "* ";
         }
-        cout << endl;
+        cout << "\n";
     }
 
-    for (int i = 1; i < n; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
+    for (int i = n - 2; i >= 0; i--) {
+        for (int j = 0; j < i; j++) {
             cout << "  ";
         }
-        for (int j = 0; j < 2 * i + 1; j++) {
+        for (int j = 0; j < (2 * n) - (2 * i) - 1; j++) {
             cout << "* ";
         }
         cout << endl;

893b32057e9bded55c5c8ffff3f9e943d869c7f1
Joe <kavvy99@naver.com>
2024-03-28 11:21:48 +0900

    Title: 열에 따라 감소하는 정사각형, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240328/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
+++ 240328/열에 따라 감소하는 정사각형/a-square-that-decreases-with-heat.cpp
@@ -6,11 +6,17 @@ int main() {
     int n;
     cin >> n;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cout << n - j << " ";
-        }
-        cout << endl;
-    }
+    // for (int i = 0; i < n; i++) {
+    //     for (int j = n; j > 0; j--) {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+    for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			cout << n - j << " ";
+		}
+		cout << endl;
+	}
     return 0;
 }
